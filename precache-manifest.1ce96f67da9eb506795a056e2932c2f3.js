0701c65c402663c83063ddeca9d13640c6360ff5
theLAZYmd <theLAZYmd@gmail.com>
2020-04-05 02:03:00 +0100

    Updates

---
+++ precache-manifest.1ce96f67da9eb506795a056e2932c2f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12b8ca141d7661322b049a8b5d2dafd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1e708d7ce5c83956c37",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd900b003ed370140874",
+    "url": "/static/js/2.e3c96171.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3c96171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1e708d7ce5c83956c37",
+    "url": "/static/js/main.c726b06c.chunk.js"
+  },
+  {
+    "revision": "85330e44f16dc4529f67",
+    "url": "/static/js/runtime-main.ebbb9f8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
