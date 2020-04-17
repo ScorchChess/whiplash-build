4706b4543fb53a10ed836a66869d9dc2f4ed8951
theLAZYmd <theLAZYmd@gmail.com>
2020-04-18 00:17:15 +0100

    Updates

---
+++ precache-manifest.73d669b5c2e911f12da2b094986903a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8571563fac0e21a2b9227cb148cf7c67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "252e6b7d2c1481398cb7",
+    "url": "/static/css/main.30e5dafd.chunk.css"
+  },
+  {
+    "revision": "d8522aa3e6e05efdf2f8",
+    "url": "/static/js/2.64f99427.chunk.js"
+  },
+  {
+    "revision": "04f7424fe823c0d6590a25e95a41c67f",
+    "url": "/static/js/2.64f99427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252e6b7d2c1481398cb7",
+    "url": "/static/js/main.780a3b5b.chunk.js"
+  },
+  {
+    "revision": "8d9ecd0f490587fba64b",
+    "url": "/static/js/runtime-main.d1d31098.js"
+  },
+  {
+    "revision": "e1f7813a81c4579705af8aec19b9aac0",
+    "url": "/static/media/twitch.e1f7813a.svg"
+  },
+  {
+    "revision": "ddf443c397b5e705c29c22174e082972",
+    "url": "/static/media/watch.ddf443c3.svg"
+  },
+  {
+    "revision": "46bc69048b59bb79de5e78c84cdf683a",
+    "url": "/static/media/youtube.46bc6904.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3899ecedda016ebc93b6e0a96efdcca7.js"
+  "/precache-manifest.73d669b5c2e911f12da2b094986903a2.js"
 );
 
 self.addEventListener('message', (event) => {
