3ffbb6b7670580829cf523e8176ec115ca4905b3
theLAZYmd <theLAZYmd@gmail.com>
2020-04-27 06:40:04 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4a53b91f940665862a80fe19d04ee21.js"
+  "/precache-manifest.12d96afb05adea85d81247a249162082.js"
 );
 
 self.addEventListener('message', (event) => {
